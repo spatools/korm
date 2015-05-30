c1dad57a6ab4105e2131d1b2601ba77b26428c7a
SomaticIT <contact@somatic.fr>
2015-05-30 04:12:36 +0200

    Use promizr base typings as more precise

--- _definitions.d.ts
+++ _definitions.d.ts
@@ -2,9 +2,10 @@
 /// <reference path="typings/knockout.mapping/knockout.mapping.d.ts" />
 /// <reference path="typings/underscore/underscore.d.ts" />
 /// <reference path="typings/moment/moment.d.ts" />
+/// <reference path="typings/requirejs/require.d.ts" />
 /// <reference path="bower_components/koutils/dist/koutils.d.ts" />
 /// <reference path="bower_components/kounderscore/dist/kounderscore.d.ts" />
-/// <reference path="bower_components/promise-ext/src/promise.d.ts" />
+/// <reference path="bower_components/promizr/promise.d.ts" />
 
 interface IDBEvent extends Event {
     target: IDBEventTarget;
--- src/base.d.ts
+++ src/base.d.ts
@@ -1,2 +1,2 @@
 /// <reference path="../typings/knockout/knockout.d.ts" />
-/// <reference path="../bower_components/promise-ext/src/promise.d.ts" />
+/// <reference path="../bower_components/promizr/promise.d.ts" />
